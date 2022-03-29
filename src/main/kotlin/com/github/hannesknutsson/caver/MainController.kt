9854b83b4f8b803fdf3f8f5e2676385e7014ae7a
Hannes Knutsson <hannes.knutsson@protonmail.com>
2022-03-29 18:24:25 +0200

    Try adding stupid shit so that maven and detekt will go crazy

--- src/main/kotlin/com/github/hannesknutsson/caver/MainController.kt
+++ src/main/kotlin/com/github/hannesknutsson/caver/MainController.kt
@@ -25,6 +25,7 @@ class MainController : WebSecurityConfigurerAdapter() {
     fun index(model: Model, principal: Principal): String {
         model.addAttribute("eventName", "FIFA 2018")
         println(principal)
+        asödlfkjaösldfk
         return "index"
     }
 }
