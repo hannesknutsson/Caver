dd3d34d0d871b0ab167d2e984476baeb5a527134
Hannes Knutsson <hannes.knutsson@protonmail.com>
2023-05-15 21:50:07 +0200

    Add spotify service for refreshing tokens and do some other garbage

--- caver-api/src/main/kotlin/com/github/hannesknutsson/caver/configuration/OAuth2Config.kt
+++ caver-api/src/main/kotlin/com/github/hannesknutsson/caver/configuration/OAuth2Config.kt
@@ -1,6 +1,6 @@
 package com.github.hannesknutsson.caver.configuration
 
-import com.github.hannesknutsson.caver.manager.UserManager
+import com.github.hannesknutsson.caver.service.UserService
 import com.github.hannesknutsson.caver.security.SpotifyAuthenticationSuccessHandler
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -9,17 +9,15 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
 
 @Configuration
 class OAuth2Config(
-        private val userManager: UserManager,
+        private val userService: UserService,
         private val oauth2Service : OAuth2AuthorizedClientService
 ) : WebSecurityConfigurerAdapter() {
 
-    @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
         http.authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
-                .successHandler(SpotifyAuthenticationSuccessHandler(userManager, oauth2Service))
+                .successHandler(SpotifyAuthenticationSuccessHandler(userService, oauth2Service))
     }
-
 }
--- caver-api/src/main/kotlin/com/github/hannesknutsson/caver/controller/IndexController.kt
+++ caver-api/src/main/kotlin/com/github/hannesknutsson/caver/controller/IndexController.kt
@@ -1,17 +1,17 @@
 package com.github.hannesknutsson.caver.controller
 
-import com.github.hannesknutsson.caver.manager.UserManager
+import com.github.hannesknutsson.caver.service.UserService
 import com.github.hannesknutsson.caver.security.AuthorizedUser
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class IndexController(
-        private val userManager: UserManager
+        private val userService: UserService
 ) {
 
     @GetMapping("/")
     fun index(): String {
-        return "Logged in as " + userManager.getUser(AuthorizedUser.getUserId()).id
+        return "Logged in as " + userService.getUser(AuthorizedUser.getUserId()).id
     }
 }
